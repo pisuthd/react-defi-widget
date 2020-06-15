d50aa8b732bab1213e89178da1af2ad872af0d5d
Pisuth D <pisuth.dae@gmail.com>
2020-06-15 23:30:12 +0700

    Feat: Summarise the data prior to funding / liquidating pools

--- src/components/widgets/stakingLiquidity.js
+++ src/components/widgets/stakingLiquidity.js
@@ -2,11 +2,14 @@ import React, { Component, useCallback, Fragment, useState, useEffect, useMemo }
 import ContainerDimensions from 'react-container-dimensions'
 import styled from 'styled-components';
 import useTheme from "../../hooks/theme"
+import useInterval from "../../hooks/interval";
+import { toFixed } from "../../utils/conversion";
 import { useLiquidityPool } from "../../hooks/liquidityPool";
 import { COLORS } from "../../constants";
 import Layout from "./layout";
 import { getIcon } from "../../utils/token";
 import { hexIsLight } from "../../utils/conversion";
+import { InsufficientAmount } from "./components/ui/alert"
 import { SummaryHeadline, Summary, SummaryContainer, Row, Column } from "./components/ui/common";
 
 const ACTION = {
@@ -21,12 +24,23 @@ const StakeLiquidity = ({
     poolSymbol
 }) => {
 
-    const { loading, tokens, setCurrentPool, poolData } = useLiquidityPool(web3ReactContext)
+    const {
+        loading,
+        tokens,
+        setCurrentPool,
+        poolData,
+        getTokenBalance,
+        getETHBalance
+    } = useLiquidityPool(web3ReactContext);
+
     const [dropdownActive, setDropdownActive] = useState(false);
-    const [actionType, setActionType] = useState(ACTION.ADD_LIQUIDITY)
+    const [actionType, setActionType] = useState(ACTION.ADD_LIQUIDITY);
+    const [balances, setBalances] = useState({});
+    const [amounts, setAmounts] = useState({});
 
     useMemo(() => {
         setCurrentPool(poolSymbol);
+        setAmounts({});
     }, [poolSymbol])
 
     const networkName = (id) => {
@@ -39,7 +53,7 @@ const StakeLiquidity = ({
             return "Unknown"
         }
     }
-    /*
+
     const totalLiquidity = useMemo(() => {
         const total = tokens.reduce((prev, item) => {
             if (item.balanceInUsd) {
@@ -49,12 +63,79 @@ const StakeLiquidity = ({
         }, 0)
         return total;
     }, [tokens])
-    */
 
-  
+    const ratio = useMemo(() => {
+        const ratio = tokens.reduce((prev, item) => {
+            let reserveRatio;
+            if (prev === "") {
+                reserveRatio = `${Math.ceil(item.ratio * 100)}`;
+            } else {
+                reserveRatio = prev + `/${Math.ceil(item.ratio * 100)}`
+            }
+            return reserveRatio;
+        }, "")
+        return ratio;
+    }, [tokens])
 
-    console.log("tokens : ", tokens, loading)
+    const poolTokenAmount = useMemo(() => {
+        let amount = 0;
+        const symbols = Object.keys(amounts);
+        for (let symbol of symbols) {
+            const { ratio, balance } = tokens.find(item => item.symbol === symbol);
+            const basePercentage = (100 * Number(amounts[symbol]) / (Number(balance) * ratio));
+            const total = (basePercentage * (ratio * poolData.totalSupply)) / 100
+            amount += Number(total * ratio);
+        }
 
+        return amount;
+
+    }, [amounts, poolData, tokens])
+
+    const updateBalance = useCallback(async () => {
+        if (tokens.length > 0) {
+            let balance = {};
+            for (let token of tokens) {
+                if (token.symbol === "ETH") {
+                    const result = await getETHBalance();
+                    balance[token.symbol] = result;
+                } else {
+                    const result = await getTokenBalance(token.address);
+                    balance[token.symbol] = result;
+                }
+            }
+            setBalances(balance);
+        }
+    }, [tokens])
+
+    const handleChange = useCallback((symbol, amount) => {
+        if (!tokens.find((item) => item.symbol === symbol)) {
+            return;
+        }
+        if (Number(poolData.totalSupply) <= 0) {
+            return;
+        }
+
+        const regexp = /^[0-9]*(\.[0-9]{0,8})?$/;
+        const previousValue = amounts[symbol] ? amounts[symbol] : 0;
+        const value = regexp.test(amount) ? (amount) : previousValue;
+        const { ratio, balance } = tokens.find(item => item.symbol === symbol);
+        const basePercentage = (100 * Number(value) / (Number(balance) * ratio))
+        let others = {};
+        const filtered = tokens.filter(item => item.symbol !== symbol)
+        for (let token of filtered) {
+            const tokenAmount = (basePercentage * (token.ratio * token.balance)) / 100
+            others[token.symbol] = toFixed(tokenAmount, 9)
+        }
+
+        setAmounts({
+            ...others,
+            [symbol]: value
+        })
+    }, [amounts, tokens, poolData])
+
+    // console.log("tokens : ", tokens, loading)
+    // console.log("pool data --> ", poolData, amounts)
+    useInterval(updateBalance, 3000)
     const isBackgroundLight = hexIsLight(color);
 
     return (
@@ -67,44 +148,95 @@ const StakeLiquidity = ({
                             height={height}
                         >
                             <h4>{poolSymbol}</h4>
-                            <p>This pool's version is outdated</p>
                             <TokensContainer
                                 width={width}
                                 height={height}
                                 totalTokens={tokens.length}
                             >
-                                {tokens.map((token, index) =>
-                                    <TokenContainer key={index}>
-                                        <InputGroup>
-                                            <InputGroupIcon>
-                                                <TokenLogo src={getIcon(token.symbol)} alt={token.symbol} />
-                                            </InputGroupIcon>
-                                            <InputGroupDropdown>
-                                                {`${token.symbol}`}
-                                            </InputGroupDropdown>
-                                            <InputGroupArea>
-                                                <input value={0} id="sourceInput" onChange={{}} placeholder="0.00" type="number" min="0" step="0.01" pattern="^\d+(?:\.\d{1,6})?$" />
-                                            </InputGroupArea>
-                                        </InputGroup>
-                                    </TokenContainer>)}
+                                {tokens.map((token, index) => {
+                                    const tokenAmount = amounts[token.symbol] || 0;
+                                    const balance = balances[token.symbol] ? toFixed(Number(balances[token.symbol]), 4) : 0;
+
+                                    return (
+                                        <Fragment>
+                                            <TokenContainer key={index}>
+                                                <InputGroup>
+                                                    <InputGroupIcon>
+                                                        <TokenLogo src={getIcon(token.symbol)} alt={token.symbol} />
+                                                    </InputGroupIcon>
+                                                    <InputGroupDropdown>
+                                                        {`${token.symbol}`}
+                                                    </InputGroupDropdown>
+                                                    <InputGroupArea>
+                                                        <input value={tokenAmount} id={`sourceInput-${token.symbol}`} onChange={(e) => handleChange(token.symbol, e.target.value)} placeholder="0.00" type="number" min="0" step="0.01" pattern="^\d+(?:\.\d{1,6})?$" />
+                                                    </InputGroupArea>
+                                                </InputGroup>
+                                                <InputBottom
+                                                    width={width}
+                                                >
+                                                    <div>
+                                                        BALANCE : {balance}{` `}{token.symbol}
+                                                    </div>
+                                                    <div>
+                                                        {0}{` `}/{` `}{Number(token.balance).toFixed(2)}{` `}{token.symbol}
+                                                    </div>
+                                                </InputBottom>
+                                                {Number(tokenAmount) > Number(balance) &&
+                                                    <InsufficientAmount />
+                                                }
+
+                                            </TokenContainer>
+                                        </Fragment>
+                                    )
+                                }
+                                )}
                                 {web3ReactContext.networkId && (networkId !== web3ReactContext.networkId) &&
                                     <div className="text-center">
                                         <p style={{ color: "red" }}>You're not connected to {networkName(networkId)}</p>
                                     </div>
                                 }
-
-
                             </TokensContainer>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    fontSize: "12px",
+                                    paddingTop: 10,
+                                }}
+                            >
+                                <div
+                                    style={{ flex: "50%" }}
+                                >
+                                    <b>Ratio: </b>{ratio}
+                                </div>
+                                {/*
+                                <div
+                                    style={{ flex: "40%", textAlign: "center" }}
+                                >
+
+                                    <b>Size: </b>${Math.floor(totalLiquidity)}
+                                </div>
+                                */}
+                                <div
+                                    style={{ flex: "50%", textAlign: "right" }}
+                                >
+                                    <b>Pool Fee: </b>{poolData.fee}%
+                                </div>
+                            </div>
                             <SummaryContainer>
                                 <SummaryHeadline
                                     width={width}
+                                    style={{ display: "none" }}
                                 >
                                     POOL INFORMATION
                                 </SummaryHeadline>
+
                                 <Summary
                                     width={width}
+                                    style={{ display: "none" }}
                                 >
+                                    {/*
                                     <Row>
+                                        
                                         <Column
                                             style={{ flex: "70%" }}
                                         >
@@ -113,15 +245,23 @@ const StakeLiquidity = ({
                                                 {0}{` `}/{` `}{poolData.totalSupply ? Number(poolData.totalSupply).toLocaleString() : "0"}{` `}{poolSymbol}
                                             </div>
                                         </Column>
+                                        
                                         <Column
-                                            style={{ flex: "30%" }}
+                                            style={{ flex: "60%" }}
+                                        >
+                                            <b>Pool's Version</b>
+
+                                        </Column>
+
+                                        <Column
+                                            style={{ flex: "40%" }}
                                         >
                                             <b>Fees</b>
                                             <div>
                                                 {poolData.fee}%
                                             </div>
                                         </Column>
-                                    </Row>
+                                    </Row>*/}
                                     {/*
                                     <Row>
                                         <Column>
@@ -143,21 +283,21 @@ const StakeLiquidity = ({
                                         return (
                                             <Row key={index}>
                                                 <Column
-                                                    style={{ flex: "70%" }}
+                                                    style={{ flex: "80%" }}
                                                 >
-                                                    <b>{item.symbol}</b><br />
-                                                    <b>Total locked</b>
                                                     <div>
-                                                        {Number(item.balance).toLocaleString()}{` `}{item.symbol}
+                                                        <TokenLogo src={getIcon(item.symbol)} style={{ width: "18px", height: "18px", marginBottom: "2px" }} alt={item.symbol} />
+                                                        {` `}
+                                                        {Number(item.balance).toFixed(2)}{` `}{item.symbol}
+                                                        {` `}
+
                                                     </div>
                                                 </Column>
                                                 <Column
-                                                    style={{ flex: "30%" }}
+                                                    style={{ flex: "20%" }}
                                                 >
-                                                    <br />
-                                                    <b>Ratios</b>
                                                     <div>
-                                                        {item.ratio}
+                                                        {Math.floor(Number(item.ratio * 100))}%
                                                     </div>
                                                 </Column>
                                             </Row>
@@ -168,7 +308,48 @@ const StakeLiquidity = ({
                                 </Summary>
 
                             </SummaryContainer>
+                            <SummaryContainer>
+                                <SummaryHeadline
+                                    width={width}
+                                >
+                                    SUMMARY
+                                </SummaryHeadline>
+                                <Summary
+                                    width={width}
+                                >
 
+                                    <Row>
+                                        <Column
+                                            style={{ flex: "50%" }}
+                                        >
+                                            <b>My stake</b>
+                                            <div>
+                                                {0}{` `}{poolSymbol}
+                                            </div>
+                                            <div>
+                                                +{poolTokenAmount.toLocaleString()}{` `}{poolSymbol}
+                                            </div>
+                                            {poolData.totalSupply && totalLiquidity &&
+                                                <div>
+                                                    (+${ Number((  totalLiquidity/ poolData.totalSupply)*(poolTokenAmount)).toLocaleString() })
+                                                </div>
+                                            }
+
+                                        </Column>
+                                        <Column
+                                            style={{ flex: "50%" }}
+                                        >
+                                            <b>Total supply</b>
+                                            <div>
+                                                {poolData.totalSupply ? Number(poolData.totalSupply).toLocaleString() : "0"}{` `}{poolSymbol}
+                                            </div>
+                                            <div>
+                                                (${totalLiquidity.toLocaleString()})
+                                            </div>
+                                        </Column>
+                                    </Row>
+                                </Summary>
+                            </SummaryContainer>
 
                             <ButtonContainer>
                                 {/*
@@ -284,6 +465,24 @@ const Dropdown = styled.div`
     a {
         cursor: pointer;
 
+    }
+
+`;
+
+const InputBottom = styled.div`
+    display: flex;
+    ${({ width }) => width >= 400 && `
+        margin-top: 5px;
+    `}
+
+    font-size: ${({ width }) => width >= 400 ? "12px" : "10px"};
+    font-weight: 500;
+
+    div {
+        flex: 50%;
+        :last-child {
+            text-align: right;
+        }
     }
 
 `;
