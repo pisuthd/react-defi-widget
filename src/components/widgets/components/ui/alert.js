15d1c00c35699292e1c098e06840759b73621a5c
Pisuth D <pisuth.dae@gmail.com>
2020-06-15 23:28:07 +0700

    Feat: Add an alert component

---
+++ src/components/widgets/components/ui/alert.js
@@ -0,0 +1,13 @@
+import React  from "react";
+
+
+
+
+
+export const InsufficientAmount = () => {
+    return (
+        <div className="text-center" style={{ fontSize: 12, color: "red", fontWeight: "600" }}>
+            Insufficient amount
+        </div>
+    )
+}
